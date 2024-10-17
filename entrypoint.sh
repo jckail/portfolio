eaba1a485f75ad503c558c1156ec264479244ecf
Jordan <jckail13@gmail.com>
2024-10-17 00:26:43 -0600

    updated entrypoin.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start the backend
-uvicorn backend.app.main:app --host 0.0.0.0 --port 8000 --reload &
+uvicorn backend.app.main:app --host 0.0.0.0 --port 8080 --reload &
 
 # Start the frontend
-cd frontend && npm run dev -- --host 0.0.0.0 --port 8080 &
+cd frontend && npm run build  && npm run dev -- --host 0.0.0.0 --port 5173 &
 
 # Wait for any process to exit
 wait -n
