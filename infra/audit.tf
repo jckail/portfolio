e51b39ec6bae3f219993278fa9a8418dfc955d38
Jordan Kail <13952435+jckail@users.noreply.github.com>
2026-09-05 13:15:32 -0700

    Use supported IAM and STS audit log service categories

--- infra/audit.tf
+++ infra/audit.tf
@@ -2,10 +2,10 @@
 # are security-sensitive; retain them privately under the project's log policy.
 resource "google_project_iam_audit_config" "sensitive_access" {
   for_each = {
-    "secretmanager.googleapis.com"  = ["DATA_READ"]
-    "sts.googleapis.com"            = ["DATA_READ"]
-    "iamcredentials.googleapis.com" = ["DATA_READ"]
-    "storage.googleapis.com"        = ["DATA_READ", "DATA_WRITE"]
+    "secretmanager.googleapis.com" = ["DATA_READ"]
+    "sts.googleapis.com"           = ["ADMIN_READ", "DATA_READ"]
+    "iam.googleapis.com"           = ["DATA_READ"]
+    "storage.googleapis.com"       = ["DATA_READ", "DATA_WRITE"]
   }
 
   project = var.project_id
